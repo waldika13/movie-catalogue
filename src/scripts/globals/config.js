131e0b9a0e73aae5516b3752a4bf17b3f711eecf
waldika <waldika.irawan00@gmail.com>
2022-04-09 20:52:08 +0700

    add websocker and notification

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+  KEY: '4d981e0f727a25f416d040e2e51263a9',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: new Date().toISOString(),
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+  WEB_SOCKET_SERVER: 'wss://movies-feed.dicoding.dev',
+};
+
+export default CONFIG;
---
+++ src/scripts/utils/websocket-initiator.js
@@ -0,0 +1,21 @@
+import CONFIG from '../globals/config';
+import NotificationHelper from './notification-helper';
+
+const WebSocketInitiator = {
+  init(url) {
+    const webSocket = new WebSocket(url);
+    webSocket.onmessage = this._onMessageHandler;
+  },
+
+  _onMessageHandler(message) {
+    const movie = JSON.parse(message.data);
+    NotificationHelper.sendNotification({
+      title: `${movie.title} is onn cinema!`,
+      options: {
+        body: movie.overview,
+        image: `${CONFIG.BASE_IMAGE_URL + movie.poster_path}`,
+      },
+    });
+  },
+};
+export default WebSocketInitiator;
